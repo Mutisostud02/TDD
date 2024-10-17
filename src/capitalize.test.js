4accd468d9c0974eecd2372b63329321acadefc8
Mutisostud02 <https://github.com/Mutisostud02>
2024-10-17 20:14:20 +0300

    Included test for empty values and non string values.

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -5,5 +5,11 @@ it('Capitalize first letter', () => {
     expect(capitalize('Chris')).toBe('Chris');
 })
 it('Capitalize first letter example 2', () => {
-    expect(capitalize('C')).toBe('C');
+    expect(capitalize('c')).toBe('C');
+})
+it('Dont capitalize if not a letter', () => {
+    expect(capitalize('45')).toBe(undefined)
+})
+it('Dont capitalize if null', () => {
+    expect(capitalize('')).toBe(undefined)
 })
