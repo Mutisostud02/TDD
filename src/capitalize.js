bba8c281abc1dfff72853b9b11b7e82addb159d2
Mutisostud02 <https://github.com/Mutisostud02>
2024-10-17 20:12:10 +0300

    Non string values would return themself
    Added logic to return undefined if value is not a string.

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,8 +1,10 @@
 function capitalize(name) {
+    if(!name.match(/[a-z]/i)) {
+        return undefined;
+    }
     let result = '';
     const convertChar = name.charAt(0).toUpperCase();
     result += convertChar + name.slice(1, name.length - 0) 
     return result;
 }
-console.log(capitalize('s'))
 module.exports = capitalize;
