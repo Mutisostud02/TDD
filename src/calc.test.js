e1aa20f56e68d67c41930efda31c345ff8401f5a
Mutisostud02 <https://github.com/Mutisostud02>
2024-10-18 17:28:24 +0300

    Added subtraction, multiplication and division tests

--- src/calc.test.js
+++ src/calc.test.js
@@ -10,3 +10,12 @@ it('Test for object return', () => {
 it('Test for add functionality', () => {
     expect(calc().add(4, 5)).toEqual(9);
 })
+it('Test for subtraction', () => {
+    expect(calc().subtract(4,5)).toEqual(1);
+})
+it('Test for multiplication', () => {
+    expect(calc().multiply(4, 5)).toEqual(20);
+})
+it('Test for division', () => {
+    expect(calc().divide(4, 5)).toEqual(0.8);
+})
