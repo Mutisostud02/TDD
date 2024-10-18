3861a861dcb40b6086eb4939c4746d2e51e677fe
Mutisostud02 <https://github.com/Mutisostud02>
2024-10-18 17:27:27 +0300

    Added logic for subtraction, multiplication and division

--- src/calc.js
+++ src/calc.js
@@ -1,12 +1,10 @@
 function calc() {
     const add = (x, y) => x + y;
     const subtract = (x, y) => {
-        if (x > y) {
-            return x - y;
-        }
         if (y > x) {
             return y - x;
         }
+        return x - y;
     }
     const multiply = (x, y) => x * y;
     const divide = (x, y) => x / y;
